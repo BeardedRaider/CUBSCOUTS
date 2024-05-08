3cfc1027461762bb2070dcda6f8a43f9a4ca0ba0
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-05-08 09:19:06 +0100

    refactor: Include Header and Footer components in App.jsx
    This commit modifies the App.jsx file to include the Header and Footer components. The Header component is added before the Routes, and the Footer component is added after the Routes. This change enhances the overall structure and layout of the application.

--- FRONTEND/App.jsx
+++ FRONTEND/App.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing routing components from react-router-dom
 
+import Header from "./components/Header"; // Importing Header component
 import Home from "./pages/shared/Home"; // Importing Home component
 import Login from "./pages/shared/Login"; // Importing Login component
 import Register from "./pages/shared/Register"; // Importing Register component
@@ -9,11 +10,14 @@ import Dashboard from "./pages/admin/Dashboard"; // Importing Dashboard componen
 import Events from "./pages/admin/Events"; // Importing Events component
 import Parent from "./pages/parent/ParentDash"; // Importing Welcome component
 import Child from "./pages/child/ChildDash"; // Importing Child component
+import Footer from "./components/Footer"; // Importing Footer component
+
 
 // Defining the App component
 function App() {
   <div>
     <Router>
+      <Header />
       <Routes>
         {/* components are renderd when the url matches the path */}
         <Route path="/" element={<Home />} />
@@ -24,6 +28,7 @@ function App() {
         <Route path="/parent" element={<Parent />} />
         <Route path="/child" element={<Child />} />
       </Routes>
+      <Footer />
     </Router>
   </div>;
 }
