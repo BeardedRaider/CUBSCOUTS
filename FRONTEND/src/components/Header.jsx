f6ba3a686c17d3c327216c3236217cecc426d7a8
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-05-08 18:30:08 +0100

    in the process of sorting out the nav bar, continue tomorrow

--- FRONTEND/src/components/Header.jsx
+++ FRONTEND/src/components/Header.jsx
@@ -3,12 +3,45 @@ import { Link, useNavigate } from 'react-router-dom';
 import '../styles/header.css';
 
 
-const Header = () => {
+const Header = ({ className }) => {
+    // Navbar component
+    const navigate = useNavigate();
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const [parent, setParent] = useState(localStorage.getItem('parent') === 'true');
+  
+    const handleLogout = () => {// Logout function
+      localStorage.removeItem('token');// Remove the token from the local storage
+      setToken(null); // Update the state
+      navigate('/');// Redirect to the home page
+    };
+  
+    
+    // Listen for changes in the local storage
+    useEffect(() => {
+      const handleStorageChange = () => {
+        setToken(localStorage.getItem('token'));
+        setParent(localStorage.getItem('parent') === 'true');
+      };
+  
+      // Attach the event listener
+      window.addEventListener('storage', handleStorageChange);
+  
+      // Detach the event listener when the component unmounts
+      return () => {
+        window.removeEventListener('storage', handleStorageChange);
+        };
+      }, []);
+  
+      // Log the changes in token and parent state
+    useEffect(() => {
+      console.log('Token has changed:', token);
+      console.log('Parents click here:', parent);
+    }, [token, parent]);
 
   return (
     <div className={`navbar`}>
       <div>
-      <img className="logo" src={process.env.PUBLIC_URL + '/images/logo.png'} alt='logo' />
+      <img  className="logo" src={process.env.PUBLIC_URL + '../../../public/images/logo.png'} alt='Cub Scouts' />
       </div>
       <div>
         <ul>
@@ -20,7 +53,6 @@ const Header = () => {
           <li><Link to="register">register</Link></li>
         </ul>
       </div>
-
     </div>
   )
 }
