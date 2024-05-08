02b2221df1deab2e21a35e48b7595a39fd2a28e6
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-05-08 08:39:08 +0100

    refactor: Include Parent and Child components in App.js routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // Importing necessary modules and components
 import React, { useState, useEffect } from 'react';
-import DesktopWarning from './components/pages/shared/DesktopWarning';
 import { BrowserRouter as Router, Route, Routes,} from "react-router-dom";// Importing routing components from react-router-dom
 
 import Home from '../FRONTEND/pages/shared/Home';// Importing Home component
