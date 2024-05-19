a74059b3b561660f03d6f507ca9e5129c6729e54
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-05-19 13:26:53 +0100

    refactor: Update Events component to include welcome message and login button

--- FRONTEND/src/pages/admin/Aevents.jsx
+++ FRONTEND/src/pages/admin/Aevents.jsx
@@ -12,10 +12,27 @@ const Events = () => {
           <h1 className="text-3xl md:text-5xl p-1 text-yellow-300 tracking-loose">Welcome
           </h1>
           <h2 className="text-3xl md:text-4xl leading-relaxed md:leading-snug mb-2 text-white"> 
-          {user ? user.name : 'Loading...'}!
+          {user ? user.name : 'Loading...'} to the events page.
           </h2>
         </div>
-      </section>    </div>
+      </section>
+      <section>
+        <div className="flex items-center justify-center py-10">
+          <div className="bg-white rounded-xl shadow-lg p-8 flex flex-col items-center">
+            <p className="text-xl text-gray-500">Events</p>
+            <h2 className="text-6xl font-bold">Coming Soon</h2>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="flex items-center justify-center bg-gray-300 py-10">
+          <div className="bg-white rounded-xl shadow-lg p-8 flex flex-col items-center">
+            <p className="text-xl text-gray-500">Events</p>
+            <h2 className="text-6xl font-bold">Coming Soon</h2>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
